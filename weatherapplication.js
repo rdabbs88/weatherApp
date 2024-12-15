205d4ed8a24c5142a82944174ef5b41a13a6a05a
Ryan Dabbs <rdabbs88@terpmail.umd.edu>
2024-12-14 22:38:51 -0500

    Set up reusable API retrieval function

--- weatherapplication.js
+++ weatherapplication.js
@@ -10,6 +10,9 @@ const collectionName = process.env.MONGO_COLLECTION;
 const bodyParser = require("body-parser");
 const app = express();
 
+const BASE_URL = "http://api.weatherstack.com/current";
+const UNIT = "f";
+
 const portNumber = 3000;
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -35,6 +38,52 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// Function to fetch weather data for a given location
+async function getWeather(zip) {
+    
+    //let temperature, city, state, windSpeed, weatherDescription, feelsLike, localTime;
+
+    try {
+      const response = await fetch(`${BASE_URL}?access_key=${process.env.API_KEY}&query=${zip}&units=${UNIT}`);
+
+      console.log("\nMake API request to WeatherStack");
+
+      // check if the API request was successful
+      if (response.ok && data.success) {
+        const data = await response.json();
+
+        const temperature = data.current.temperature;
+        const city = data.location.name;
+        const state = data.location.region;
+        const windSpeed = data.current.wind_speed;
+        const weatherDescription = data.current.weather_descriptions;
+        const feelsLike = data.current.feelslike;
+        const localTime = data.location.localtime;
+
+        return {
+            "temperature": temperature,
+            "city": city, 
+            "state": state,
+            "windSpeed": windSpeed,
+            "weatherDescription": weatherDescription,
+            "feelsLike": feelsLike,
+            "localTime": localTime
+        };
+
+      }
+      else {   // if API request for zip code didn't work (invalid ZIP)
+        console.log("INVALID ZIP, ENTERED ELSE");
+        return -1;
+      }
+    } 
+    catch (error) {
+        console.log("ENTERED CATCH");
+        //console.error("Error:", error);
+        return -1;
+    }
+  
+  }
+
 // Project routing below
 
 // GET request to our index page
@@ -44,6 +93,31 @@ app.get("/", (request, response) => {
 
 app.get("/search", (request, response) => {
     response.render("search");
+});
+
+app.post("/searchresults", async (request, response) => {
+
+    const {zip} = request.body;
+
+    // call getWeather function to make a request to the API to grab the data we need
+    
+    const weatherDataObj = await getWeather(zip);
+
+    if (weatherDataObj == -1) {
+        response.render("ziperror");
+    }
+    else{
+        console.log(`Weather in ${zip}: ${weatherDataObj.temperature}`);
+        console.log(`city: ${weatherDataObj.city}`);
+        console.log(`state: ${weatherDataObj.state}`);
+        console.log(`Wind speed: ${weatherDataObj.windSpeed}`);
+        console.log(`Weather Description: ${weatherDataObj.weatherDescription}`);
+        console.log(`Feels Like: ${weatherDataObj.feelsLike}`);
+        console.log(`Local Time: ${weatherDataObj.localTime}`);
+
+        response.render("searchresults", weatherDataObj);
+    }
+    
 });
 
 app.get("/add", (request, response) => {
